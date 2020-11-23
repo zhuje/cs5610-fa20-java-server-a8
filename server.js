3eed0ea41d4631e2027c96847c0b381d2a37e581
zhuje <59589720+zhuje@users.noreply.github.com>
2020-11-23 01:32:00 -0500

    first commit

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require("express")
 const app = express()
 
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With, Origin');
+    res.header('Access-Control-Allow-Methods',
+               'GET, POST, PUT, PATCH, DELETE, OPTIONS'); next();
+});
+
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
 
 require("./services/quizzes-service")(app)
 require("./services/questions-service")(app)
